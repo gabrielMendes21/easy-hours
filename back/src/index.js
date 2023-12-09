370a60c67a2b69bbe09e85e9a256562edcb908b4
Gabriel Mendes <gabriel.26056@gmail.com>
2023-12-09 20:28:37 -0300

    add studentRoutes

--- back/src/index.js
+++ back/src/index.js
@@ -1,24 +1,22 @@
 // IMPORTS
 
 import cors from 'cors'
+import express from 'express'
+import studentsRoute from './routes/users/students.js'
 
 // CONFIGS
 
 const app = express()
 
-app.use(express.json())
 app.use(cors())
-
 
 // ROUTES
 
-app.get('/', (req, res) => {
-    return res.json({ msg: "You are in route '/'" })
-})
+// student
+app.use(studentsRoute)
 
 
 // SERVER
-
 const PORT = 3000
 app.listen(PORT, () => console.log("Server is running on port " + PORT))
 
