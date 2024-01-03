bc2dd1349cb839a605602140dc39504324b2a2b1
Gabriel Mendes <gabriel.26056@gmail.com>
2024-01-03 16:18:40 -0300

    add task correction route

---
+++ back/src/routes/supportRequests/supportRequests.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import prisma from '../../lib/prisma.js'
+
+const router = express.Router()
+
+// ROUTE -> GET ALL SUPPORT REQUESTS
+// ONLY AMS COORDINATOR CAN ACCESS THIS ROUTE, BECAUSE HE CAN SEE ALL SUPPORT REQUESTS
+router.get('/solicitacoes-de-suporte', async (req, res) => {
+    try {
+        // GET ALL SUPPORT REQUESTS FROM DATABASE
+        const supportRequests = await prisma.solicitacaoSuporte.findMany({
+            include: {
+                Resposta: true
+            }
+        })
+
+        return res.json({
+            msg: "Listar solicitações de suporte",
+            supportRequests
+        })
+    } catch(error) {
+        return res.status(500).json({
+            msg: "Listar solicitações de suporte",
+            error
+        })
+    }
+})
+
+// ROUTE - GET SUPPORT REQUESTS FROM A SPECIFIC STUDENT
+router.get('/aluno/:studentId/solicitacoes-de-suporte', async (req, res) => {
+    try {
+        // GET STUDENT ID
+        const { studentId } = req.params
+
+        // GET ONLY SUPPORT REQUESTS WHOSE CREATOR IS THE STUDENT WHO HAS THE ID PROVIDED
+        const studentSupportRequests = await prisma.solicitacaoSuporte.findMany({
+            where: {
+              codAluno: Number(studentId),
+            },
+            include: {
+              Resposta: true,
+            },
+          })
+
+          return res.json({
+            msg: "Listar solicitações de suporte do aluno",
+            studentSupportRequests
+          })
+    } catch(error) {
+        return res.status(500).json({
+            msg: "Listar solicitações de suporte do aluno",
+            error
+        })
+    }
+})
+
+export default router
---
+++ back/src/routes/taskCorrection/taskCorrection.js
@@ -0,0 +1,89 @@
+import express from 'express'
+import prisma from '../../lib/prisma.js'
+
+const router = express.Router()
+
+// ROUTE -> CREATE A CORRECTION FOR A DELIVERED TASK
+/*
+    REQUEST BODY EXAMPLE
+    {
+        "assessment": "Great job",
+        "taskVal": "approved"
+    }
+*/
+router.post('/correcao', async (req, res) => {
+    try {
+        // GET DATA FROM REQUEST BODY
+        const data = req.body
+
+        // GET TASK DELIVERY ID AND COORDINTAOR ID FROM QUERY PARAMS
+        const { taskDeliveryId, coordinatorId } = req.query
+
+        // TRUE => TASK IS APPROVED
+        // FALSE => TASK IS NOT APPROVED
+        const isTaskApproved = data.taskVal === 'approved'
+
+        // CREATE A CORRECTION IN DATABSE
+        await prisma.correcao.create({
+            data: {
+                codEntrega: Number(taskDeliveryId),
+                conteudo: data.assessment,
+                codCoordenador: Number(coordinatorId),
+                entregaAprovada: !!isTaskApproved,
+            },
+        })
+
+        // FIND TASK DELIVERY
+        const taskDelivery = await prisma.entrega.findFirst({
+            where: {
+                id: Number(taskDeliveryId),
+            },
+            include: {
+                atividade: true,
+                aluno: {
+                    include: {
+                        Horas: true,
+                    },
+                },
+            },
+        })
+
+        // ADD HOURS TO STUDENT IF TASK WAS APPROVED BY THE COORDINATOR
+        if (isTaskApproved) {
+            // GET STUDENT HOURS OF THE CURRENT YEAR
+            const hours = taskDelivery.aluno.Horas.find((hours) => {
+                return hours.ano === new Date().getFullYear()
+            })
+
+            // STUDENT COMPLETED HOURS
+            const completedHours = hours.horasConcluidas
+
+            // ADD TASK HOURS TO STUDNET COMPLETED HOURS
+            await prisma.horas.update({
+                data: {
+                    horasConcluidas: completedHours + taskDelivery.atividade.horasAtividade,
+                },
+                where: {
+                    id: hours.id,
+                },
+            })
+        }
+
+        const responseData = {
+            correcaoDaAtividade: isTaskApproved ? "Atividade aprovada" : "Atividade reprovada",
+            horasDaAtividade: taskDelivery.atividade.horasAtividade
+        }
+
+        return res.json({
+            msg: "Correção da atividade",
+            dadosDaCorrecao: responseData
+        })
+    } catch(error) {
+        return res.status(500).json({
+            msg: "Correção de atividade",
+            error
+        })
+    }
+})
+
+export default router
